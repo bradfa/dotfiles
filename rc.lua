70fb23f0dfc5d71cb7a0e75000fdc5027dffa43a
Andrew Bradford <andrew.bradford@softiron.com>
2020-07-08 06:24:58 -0400

    rc.lua: screen lock with super-F12

--- rc.lua
+++ rc.lua
@@ -247,6 +247,8 @@ root.buttons(gears.table.join(
 
 -- {{{ Key bindings
 globalkeys = gears.table.join(
+    awful.key({ modkey,           }, "f12",
+              function () awful.spawn{ "xscreensaver-command -lock" } end),
     awful.key({ modkey,           }, "s",      hotkeys_popup.show_help,
               {description="show help", group="awesome"}),
     awful.key({ modkey,           }, "Left",   awful.tag.viewprev,
